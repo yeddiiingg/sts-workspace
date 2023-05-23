3ca29713e5361234412ffbf79d36cf9696625379
yeddy\yedy <yeji6973@naver.com>
2023-05-23 12:02:36 +0900

    sendRedirect를 이용한 리다이렉트 실습

---
+++ chap08/src/main/java/sec01/ex01/SecondServlet.java
@@ -0,0 +1,27 @@
+package sec01.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet({ "/second" })
+public class SecondServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		response.setContentType("text/html; charset=euc-kr");
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>");
+		out.println("sendRedirect를 이용한 리다이렉트 실습");
+		out.println("</body></html>");
+	}
+
+
+}
